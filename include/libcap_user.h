f4297fdb0d6e4708ec0f8f36a9acac67aa3e4a1e
David M. Johnson <johnsond@flux.utah.edu>
2015-11-04 17:24:30 -0700

    Ugh, move mutex impls into internal headers.
    (For now we don't need to abstract mutexes into public libcap.h .)

--- include/libcap_user.h
+++ include/libcap_user.h
@@ -15,33 +15,4 @@ CAP_BUILD_CORE_TYPES_NOBUILTIN();
 #define __cap_debug(format,...) \
     fprintf(stderr,"CDEBUG: %s:%d: "format,__FUNCTION__,__LINE__,## __VA_ARGS__)
 
-/**
- * Mutex support.  Follow kernel return convention.
- */
-typedef pthread_mutex_t cap_mutex_t;
-static inline int __cap_mutex_init(cap_mutex_t * mutex)
-{
-	return -pthread_mutex_init(mutex, NULL);
-}
-
-static inline int __cap_mutex_lock(cap_mutex_t * mutex)
-{
-	return -pthread_mutex_lock(mutex);
-}
-
-static inline int __cap_mutex_trylock(cap_mutex_t * mutex)
-{
-	return -pthread_mutex_trylock(mutex);
-}
-
-static inline int __cap_mutex_lock_interruptible(cap_mutex_t * mutex)
-{
-	return -pthread_mutex_lock(mutex);
-}
-
-static inline int __cap_mutex_unlock(cap_mutex_t * mutex)
-{
-	return -pthread_mutex_unlock(mutex);
-}
-
 #endif /* __LIBCAP_USER_H__ */
