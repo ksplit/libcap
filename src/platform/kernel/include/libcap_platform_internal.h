4b5f78497b4e6ba537aa5776a9312ff884f8a8a7
Charlie Jacobsen <charlie.jacobsen@utah.edu>
2016-03-01 23:35:34 -0700

    Bug in kernel platform trylock definition.
    Unlike other kernel functions, mutex_trylock returns 1 on sucess
    and 0 on error. The double negations were inverting the interpretation
    of the result. (It still worked because on the first attempt, we
    interpreted as a failure and tried again; on the second attempt, it
    was really a failure to take the lock but we interpreted as a
    success.)

--- src/platform/kernel/include/libcap_platform_internal.h
+++ src/platform/kernel/include/libcap_platform_internal.h
@@ -39,7 +39,7 @@ static inline int __cap_mutex_lock(cap_mutex_t *mutex)
 
 static inline int __cap_mutex_trylock(cap_mutex_t *mutex)
 {
-	return !mutex_trylock(mutex);
+	return mutex_trylock(mutex);
 }
 
 static inline int __cap_mutex_lock_interruptible(cap_mutex_t *mutex)
